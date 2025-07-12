b5423ddc1299977a34b8f3a69e652b2438be5362
artvilson <art@artvilsonmedia.com>
2025-07-12 22:45:03 +0400

    Исправление JSX синтаксической ошибки в HeroSection

--- src/components/home/HeroSection.tsx
+++ src/components/home/HeroSection.tsx
@@ -2,8 +2,6 @@
            </motion.div>
          </motion.div>
 
-+        </div>
-+
          <div className="max-w-4xl mx-auto text-center">
            {/* CTA Button */}
            <motion.div
