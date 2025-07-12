cc43dba95fd64ac19a3ec3c6c37aa7a1db6bed34
artvilson <art@artvilsonmedia.com>
2025-07-12 22:43:26 +0400

    Исправление незакрытого div элемента в HeroSection

---
+++ src/components/home/HeroSection.tsx
@@ -0,0 +1,9 @@
+@@ .. @@
+           </motion.div>
+         </motion.div>
+
++        </div>
++
+         <div className="max-w-4xl mx-auto text-center">
+           {/* CTA Button */}
+           <motion.div
