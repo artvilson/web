0699a04e4a48b56f85e1422f4ac1d9d92678f6fc
artvilson <art@artvilsonmedia.com>
2025-07-13 00:07:32 +0400

    Обновление chat_id для Telegram

--- artvilson-website-main/src/components/home/DemoCallForm/hooks/useFormState.ts
+++ artvilson-website-main/src/components/home/DemoCallForm/hooks/useFormState.ts
@@ -63,7 +63,7 @@ export const useFormState = ({ executeRecaptcha }: UseFormStateProps) => {
     try {
       // Отправка в Telegram
       const telegramBotToken = '7769253092:AAF7SVMgAWuMYt51KW8BOWoHBKV3Wi2_xtE';
-      const chatId = '7769253092'; // Используем ID бота как временное решение
+      const chatId = '192085032';
       
       const message = `
 🔔 Новая заявка с сайта!
